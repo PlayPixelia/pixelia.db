14e213e0ef55d632ab703fced346a7a67249089e
Real Queaxtra <60826916+Queaxtra@users.noreply.github.com>
2024-02-09 22:35:47 +0300

    🎏 Ufak tefek hata düzeltmeleri.

--- src/commands/delete.js
+++ src/commands/delete.js
@@ -4,7 +4,15 @@ module.exports = function deleteKey(database, databasePath, key) {
     if (!key) throw new Error("[HATA] Girilen herhangi bir veri belirtilmemiş.")
     if (typeof key !== "string") throw new TypeError("[HATA] Veri bir string olmalıdır.");
 
-    delete database[key];
+    if (key.includes('.')) {
+        const [parentKey, childKey] = key.split('.');
+        if (!database[parentKey] || typeof database[parentKey] !== "object") {
+            return database;
+        }
+        delete database[parentKey][childKey];
+    } else {
+        delete database[key];
+    }
     fs.writeFileSync(databasePath, JSON.stringify(database, null, 2));
     return database;
 };
